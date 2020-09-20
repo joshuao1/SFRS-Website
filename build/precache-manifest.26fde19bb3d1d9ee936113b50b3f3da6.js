4d5c9ac67991948e111453843120ef61fb690ed4
Joshua Ong <joshuao1@student.unimelb.edu.au>
2020-09-20 13:48:26 +1000

    build for heroku, changing to express server

---
+++ build/precache-manifest.26fde19bb3d1d9ee936113b50b3f3da6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fade52db897af42aba5887532611b206",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb6bbf6947a2a0057da0",
+    "url": "/static/js/2.2011ceae.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2011ceae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de9acfeff548a427fac0",
+    "url": "/static/js/main.8e9ad44b.chunk.js"
+  },
+  {
+    "revision": "d6368b5ecd134c27111d",
+    "url": "/static/js/runtime-main.f141e9ec.js"
+  },
+  {
+    "revision": "10ca9cdc6cd2384a1dca299fe826c0bd",
+    "url": "/static/media/IGM.10ca9cdc.jpg"
+  },
+  {
+    "revision": "ce1649421ae751ab94f3a0d77a0bbd7f",
+    "url": "/static/media/JoshuaOng_Headshot.ce164942.jpg"
+  },
+  {
+    "revision": "771d0e4d498bf3debf0db6f464437e13",
+    "url": "/static/media/UMSUnobg.771d0e4d.png"
+  },
+  {
+    "revision": "8aab010c8454f4a28a7589502282ea00",
+    "url": "/static/media/graduated.8aab010c.svg"
+  },
+  {
+    "revision": "e7582d9bd4b047855aad94612a99d908",
+    "url": "/static/media/grass.e7582d9b.svg"
+  },
+  {
+    "revision": "a81109727be4e9767a1cd6c9be837b45",
+    "url": "/static/media/group.a8110972.svg"
+  },
+  {
+    "revision": "9011b4b22e956a097de8695307a9a0ab",
+    "url": "/static/media/network.9011b4b2.svg"
+  },
+  {
+    "revision": "73dbadf9528e74c54ae28c671a42a78f",
+    "url": "/static/media/refugee_legal_banner.73dbadf9.jpg"
+  },
+  {
+    "revision": "e98ac1a833a34f13158dfd7ddb72dcf4",
+    "url": "/static/media/sfrs logo cropped.e98ac1a8.jpg"
+  },
+  {
+    "revision": "42145f37e02e339e71632296d16c2e56",
+    "url": "/static/media/sfrs oweek 2019.42145f37.png"
+  },
+  {
+    "revision": "7e41aa4c5ed87d70155e6064ac365618",
+    "url": "/static/media/sfrs trivia night.7e41aa4c.png"
+  }
+]);
